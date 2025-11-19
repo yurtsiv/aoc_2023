380028cd9f6953acfc25e5f2f252b27e6f9ac570
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2025-11-19 21:06:19 +0100

    tmp

---
+++ src/day12_part1.rs
@@ -0,0 +1,2 @@
+// Bruteforce: Generate all possible positions in which the first number could "fit". Then recursively do it for every other number.
+// Find all possible contiguous borken areas of a given size.
